97b16323ddd91e476f7ad49142df0058e61a7381
Zane Rockenbaugh <zane@liquid-labs.com>
2024-02-09 12:31:40 -0600

    sort the root data so it appears in a consistent order in index and docs

--- helpers/ddata.js
+++ helpers/ddata.js
@@ -1,3 +1,5 @@
+var handlebars = require('handlebars')
+
 function getKind(globals, index) {
   return globals[index].kind
 }
@@ -14,7 +16,36 @@ function showThis() {
   return JSON.stringify(this, null, '  ')
 }
 
+function sortAll(options) {
+  options.data.root.sort((a, b) => {
+    const indexAKind = _kinds.findIndex((k) => k.kind === a.kind)
+    const indexBKind = _kinds.findIndex((k) => k.kind === b.kind)
+
+    if (indexAKind < indexBKind) { return -1 }
+    else if (indexAKind > indexBKind) { return 1 }
+    else { return a.name.localeCompare(b.name) }
+  })
+}
+
+const _kinds = [
+  { kind: 'class', title: 'Classes' },
+  { kind: 'mixin', title: 'Mixins' },
+  { kind: 'member', title: 'Members' },
+  { kind: 'namespace', title: 'Namespaces' },
+  { kind: 'constant', title: 'Constants' },
+  { kind: 'component', title: 'Components' },
+  { kind: 'function', title: 'Functions' },
+  { kind: 'event', title: 'Events' },
+  { kind: 'typedef', title: 'Typedefs' },
+  { kind: 'external', title: 'Externals' },
+  { kind: 'interface', interface: 'Interfaces' }
+]
+
+function kinds() { return _kinds }
+
 exports.getKind = getKind
 exports.hasMultipleKinds = hasMultipleKinds
+exports.kinds = kinds
 exports.kindCount = kindCount
 exports.showThis = showThis
+exports.sortAll = sortAll
