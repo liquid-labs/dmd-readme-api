543f2c72fc944199c9ca8863962c86d1cdbf0d1d
Zane Rockenbaugh <zane@liquid-labs.com>
2024-02-09 14:45:45 -0600

    added link to source code for funsies

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1,3 +1,19 @@
+const cwd = process.cwd()
+
+function extractSourceLink() {
+  const meta = this.meta
+  const filename = meta.filename
+  const path = meta.path
+  const lineno = meta.lineno
+  // assumes we've checked out to a folder matching myPackageName
+  // TODO: this logic is assuming *nix style path separators
+  const relPath = '.' + path.slice(cwd.length)
+
+  // return JSON.stringify(path.match(new RegExp('^.+?/' + myPackageName + '(/.+)$')), null, '  ')
+
+  return relPath + '/' + filename + '#L' + lineno
+}
+
 function extractSummary(description) {
   // No idea why, but the 'm' (multiline match) doesn't seem to be working...
   return description.match(/([^.]+\.)(?:.|\n)*/m)[1] || description
@@ -11,6 +27,7 @@ function or() {
   return Array.prototype.slice.call(arguments, 0, -1).some(Boolean);
 }
 
+exports.extractSourceLink = extractSourceLink
 exports.extractSummary = extractSummary
 exports.and = and
 exports.or = or
