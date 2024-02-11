f313b62a3c7fb7038b191eb36ae60481de07d90c
Zane Rockenbaugh <zane@liquid-labs.com>
2024-02-11 10:35:25 -0600

    tested all ddata helpers

--- helpers/test/ddata.test.js
+++ helpers/test/ddata.test.js
@@ -11,6 +11,12 @@ const singleKind = [
   { kind: 'constant', name: 'aConstant' }
 ]
 
+const inOrderKinds = [
+  { kind: 'constant', name: 'aConstant' },
+  { kind: 'constant', name: 'aTestConstantEntry' },
+  { kind: 'function', name: 'aFunction' }
+]
+
 describe('handledKinds', () => {
   test('returns an array of strings', () => {
     const result = handledKinds()
@@ -24,4 +30,30 @@ describe('hasMultipleKinds', () => {
   test('recognizes multiple kinds', () => expect(hasMultipleKinds(multiKinds)).toBe(true))
 
   test('recognizes a single kind', () => expect(hasMultipleKinds(singleKind)).toBe(false))
+})
+
+describe('sortAll', () => {
+  test('sorts set with single kind', () => {
+    const data = structuredClone({ data: { root: singleKind }})
+    sortAll(data)
+    const sortedSet = data.data.root
+    expect(sortedSet).toHaveLength(2)
+    expect(sortedSet[0].name).toBe('aConstant')
+  })
+
+  test('sorts set with multiple kinds', () => {
+    const data = structuredClone({ data: { root: multiKinds }})
+    sortAll(data)
+    const sortedSet = data.data.root
+    expect(sortedSet).toHaveLength(3)
+    expect(sortedSet[0].name).toBe('aConstant')
+    expect(sortedSet[2].name).toBe('aFunction')
+  })
+
+  test('no change when set in order', () => {
+    const data = structuredClone({ data: { root: inOrderKinds }})
+    sortAll(data)
+    const sortedSet = data.data.root
+    expect(sortedSet).toEqual(inOrderKinds)
+  })
 })
